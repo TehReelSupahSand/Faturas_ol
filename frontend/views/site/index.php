46e27c51d729bccb6ca8bf754f3d14cf589300a0
Goku-san <contas.jogos.servicos@gmail.com>
2018-01-26 11:52:03 +0000

    index alterado para user logado n consseguir aceder ao login

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -15,11 +15,24 @@ $this->title = 'Faturas.OL';
     <div class="container" align="center">
         <h1>Bem-vindo ao</h1>
         <img  height="50%" width="50%" style=" margin:1em 0;  display: block" src="<?= Yii::$app->request->baseUrl ?>/logo/logogud.png">
-        <p>Também pode obter a nossa aplicação na Google Play Store.</p>
+    <?php
+        if (Yii::$app->user->isGuest) { ?>
+
+            <h4>Saiba qual o <a href='index.php/site/about'>propósito deste programa</a>, faça <a href='index.php/site/login'>login</a> caso possua uma conta
+            ou <a href='index.php/site/signup'>registe-se</a> caso queira aderir ao programa.</h4>
+     <?php   } else { ?>
+
+            <h4>Saiba qual o <a href="index.php/site/about">propósito deste programa</a>.</h4>
+       <?php }
+    ?>
     </div>
 
-    <div class="container"  align="center" style="margin: 1em 0;; ">
-        <p> <a class=" btn btn-lg btn-primary" href="index.php/site/login"> Leva-me ao Login</a> </p>
-    </div>
+    <?php
+        if (Yii::$app->user->isGuest) { ?>
 
+            <div class="container"  align="center" style=\"margin: 1em 0;; \">
+            <h4> <a class=" btn btn-lg btn-primary" href="index.php/site/login"> Leva-me ao Login</a> </h4>
+            </div>
+    <?php }
+    ?>
 </div>
